3ee0256a3e4dc358e32d1af74029bd9a1db2c143
Jérôme <jerome@fourmond.fr>
2015-10-12 15:47:39 +0200

    Une petite histoire en t'attendant

--- src/bridgeconstructor/Main.java
+++ src/bridgeconstructor/Main.java
@@ -6,7 +6,9 @@ public class Main {
 		// TODO Auto-generated code, ou un truc dans ce genre, je suis pas sûr.
 		
 		System.out.println("Il était une fois...");
-		
+		System.out.println("Un petit Florian...");
+		System.out.println("Qui...");
+		System.out.println("galérait avec Git...");
+		System.out.println("\"Git Commit ou Git Pull\" disait-il...");
 	}
-
 }
