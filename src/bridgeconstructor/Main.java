d6c66f76043bafa4776ef33914e96e471eef4299
Jérôme <jerome@fourmond.fr>
2015-10-12 15:31:04 +0200

    Hello + i

--- src/bridgeconstructor/Main.java
+++ src/bridgeconstructor/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
-		System.out.println("Hello");
+		Bridge B = new Bridge();
+		
+		for(int i = 0 ; i < 10 ; i++)
+			System.out.println("Hello" + i);
 	}
 
 }
