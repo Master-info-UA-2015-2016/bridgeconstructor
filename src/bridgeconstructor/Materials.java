b5819fc174025e6b5daef38b8e9255d3b8a5dd85
Jérôme <jerome@fourmond.fr>
2015-10-12 15:54:30 +0200

    Arrivée de la classe Materials (à revoir peut-etre)

---
+++ src/bridgeconstructor/Materials.java
@@ -0,0 +1,15 @@
+package bridgeconstructor;
+
+public class Materials {
+	public static int getSteelPrice() {
+		return 50;
+	}
+	
+	public static int getRockPrice() {
+		return 35;
+	}
+	
+	public static int getWoodPrice() {
+		return 10;
+	}
+}
