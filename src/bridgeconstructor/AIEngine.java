bac50d8ae4d697872fd7ba6b86c2d54ad6a7f0c4
Jérôme <jerome@fourmond.fr>
2015-10-12 17:01:41 +0200

    merge ?

--- src/bridgeconstructor/AIEngine.java
+++ src/bridgeconstructor/AIEngine.java
@@ -1,5 +1,9 @@
 package bridgeconstructor;
 
+/**
+ * La classe utilisera les règles ainsi que la base de faits
+ * @see Environment pour décider quel pont scierait à la base
+ */
 public class AIEngine {
-
+	//TODO remplir la classe
 }
--- src/bridgeconstructor/Environment.java
+++ src/bridgeconstructor/Environment.java
@@ -1,5 +1,22 @@
 package bridgeconstructor;
 
+/**
+ * La classe sera instanciée puis remplie par l'utilisateur
+ * en fonction de sa base de faits
+ */
 public class Environment {
-
+	//TODO Finir Remplir Classe
+	// Traffic
+	private boolean naval_traffic;
+	private boolean railway_traffic;
+	private boolean	pedestrian_traffic;
+	private boolean road_traffic;
+	
+	// Risques Météorologiques
+	private boolean storm;
+	private boolean fire;
+	private boolean flood;
+	
+	public Environment() {}
+	
 }
