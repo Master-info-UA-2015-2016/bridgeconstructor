fc544fe249e94e1b6f83992652fc8b8efd0aca47
flodavid <fl.david.53@gmail.com>
2015-10-16 11:19:14 +0200

    AiENgine

--- src/bridgeconstructor/AIEngine.java
+++ src/bridgeconstructor/AIEngine.java
@@ -4,6 +4,13 @@ package bridgeconstructor;
  * La classe utilisera les règles ainsi que la base de faits
  * @see Environment pour décider quel pont scierait à la base
  */
+
 public class AIEngine {
 	//TODO remplir la classe
+	
+	public boolean forwardChaining(){
+		
+		
+		return true;
+	}
 }
