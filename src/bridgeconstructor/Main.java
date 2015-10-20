df5df63ca81131cffb95f75a43535990511f56b2
flodavid <fl.david.53@gmail.com>
2015-10-20 20:10:40 +0200

    test dans main

--- src/bridgeconstructor/Main.java
+++ src/bridgeconstructor/Main.java
@@ -1,5 +1,7 @@
 package bridgeconstructor;
 
+import expertsystem.Affirmation;
+import expertsystem.FactsBase;
 
 public class Main {
 
@@ -10,7 +12,22 @@ public class Main {
 //		Créer un Bridge pour chaque ponts possible,
 //		contenant chaque matériau utilisable pour ce type de ponts ?
 //		on supprime le matériau utilisable ou le Bridge entier si il n'est plus envisagé ?
+	
+//###################
+//		Tests
+//##################
+		FactsBase BF= new FactsBase();
+		Affirmation a1= new Affirmation("fire", true);
+		Affirmation a2= new Affirmation("fire", true);
 		
+		BF.addFact(a1);
+		
+		System.out.println("test : "+ a1 +" egal "+ a2 +" ? "+ a1.equals(a2) );
+		System.out.println("test : "+ BF +" contient "+ a1 +" ? "+ BF.contains(a1) );
+
+//###################
+//		Fin Tests
+//##################
 		Materials materiaux = new Materials();
 		System.out.println("Prix des matériaux");
 
