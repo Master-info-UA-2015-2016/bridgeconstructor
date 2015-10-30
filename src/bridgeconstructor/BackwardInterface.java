3a0890f669087ad407a56204fa9759feb59eebe7
jfourmond <jerome@fourmond.fr>
2015-10-30 22:29:18 +0100

    RadioButton Arrière OU Mixte + Systemoutprintlnparcemoncolleguelapavu

--- src/bridgeconstructor/BackwardInterface.java
+++ src/bridgeconstructor/BackwardInterface.java
@@ -5,11 +5,13 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import expertsystem.AIEngine;
@@ -23,6 +25,13 @@ public class BackwardInterface extends JFrame implements ActionListener {
 	private static String title = "Bridge Constructor - Chaînage arrière";
 	
 	private JPanel main_panel;
+	private JPanel up_panel;
+		private JPanel up_up_panel;
+			private JLabel chaining_choice;
+		private JPanel up_center_panel;
+			private ButtonGroup radio_group;
+				private JRadioButton backward_radio;
+				private JRadioButton mixt_radio;
 	private JPanel center_panel;
 		private JLabel fact;
 		private JTextField fact_field;
@@ -45,6 +54,15 @@ public class BackwardInterface extends JFrame implements ActionListener {
 
 	public void buildComposants() {
 		main_panel = new JPanel(new BorderLayout());
+		up_panel = new JPanel(new BorderLayout());
+			up_up_panel = new JPanel();
+				chaining_choice = new JLabel("Choix du chaînage : ");
+			up_center_panel = new JPanel(new GridLayout(0, 1));
+				radio_group = new ButtonGroup();
+					backward_radio = new JRadioButton("Arrière");
+					mixt_radio = new JRadioButton("Mixte");
+				radio_group.add(backward_radio);
+			    radio_group.add(mixt_radio);
 		center_panel = new JPanel(new GridLayout(1, 1));
 			fact = new JLabel("But : ");
 			fact_field = new JTextField();
@@ -53,11 +71,18 @@ public class BackwardInterface extends JFrame implements ActionListener {
 	}
 	
 	public void buildInterface() {
-		center_panel.add(fact);
-		center_panel.add(fact_field);
+				up_up_panel.add(chaining_choice);
+				up_center_panel.add(backward_radio);
+				up_center_panel.add(mixt_radio);
+			up_panel.add(up_up_panel, BorderLayout.NORTH);
+			up_panel.add(up_center_panel, BorderLayout.CENTER);       
+				
+			center_panel.add(fact);
+			center_panel.add(fact_field);
 		
-		bottom_panel.add(confirm);
+			bottom_panel.add(confirm);
 		
+		main_panel.add(up_panel, BorderLayout.NORTH);
 		main_panel.add(center_panel, BorderLayout.CENTER);
 		main_panel.add(bottom_panel, BorderLayout.SOUTH);
 		
@@ -65,6 +90,9 @@ public class BackwardInterface extends JFrame implements ActionListener {
 	}
 	
 	public void buildEvents() {
+		backward_radio.addActionListener(this);
+		mixt_radio.addActionListener(this);
+		
 		fact_field.addActionListener(this);
 		confirm.addActionListener(this);
 		
@@ -76,10 +104,17 @@ public class BackwardInterface extends JFrame implements ActionListener {
 		Object O = E.getSource();
 		if(O.getClass() == JButton.class) {
 			JButton B = (JButton) O;
-			if(B == confirm) launchBackwardChaining();
+			if(B == confirm) {
+				if(backward_radio.isSelected())
+					launchBackwardChaining();
+				else if(mixt_radio.isSelected())
+					launchMixtChaining();
+			}
 		} else if(O.getClass() == JTextField.class) {
 			JTextField TF = (JTextField) O;
 			if(TF == fact_field) launchBackwardChaining();
+		} else if(O.getClass() == JRadioButton.class) {
+			System.out.println(O);
 		}
 		
 	}
@@ -90,6 +125,8 @@ public class BackwardInterface extends JFrame implements ActionListener {
 			JOptionPane.showMessageDialog(null, "Aucune saisie effectuée", "Bridge Constructor - Alert" , JOptionPane.ERROR_MESSAGE);
 			return;
 		}
+		
+		System.out.println("Lancement Chaînage Arrière");
 		
         // Base de Faits
 		FactsBase FB = Environment.getFactsBase();
@@ -113,5 +150,15 @@ public class BackwardInterface extends JFrame implements ActionListener {
              System.out.println("\n"+
                     "Impossible de déduire "+ goalName);
         }
+	}
+	
+	private void launchMixtChaining() {
+		String goalName = fact_field.getText();
+		if(goalName.equals("")) {
+			JOptionPane.showMessageDialog(null, "Aucune saisie effectuée", "Bridge Constructor - Alert" , JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+		System.out.println("Lancement Chaînage Mixte");		
 	}
 }
