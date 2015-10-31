b057767b37cff34b021a1f41a49ecc8023093c3a
Florian <fl.david.53@gmail.com>
2015-10-31 23:33:54 +0100

    Changement style interface

--- src/bridgeconstructor/Main.java
+++ src/bridgeconstructor/Main.java
@@ -1,11 +1,44 @@
 package bridgeconstructor;
 
+// DEFINITION DU STYLE GENERAL DU PROGRAMME
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Florian
  */
 public class Main {
 
+    public void setSystemLookAndFeel(){
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(  null,
+                    "System look and feel class not found.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(  null,
+                    "System look and feel unsupported (weird isn't it ^^ ?)...",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (InstantiationException e) {
+            JOptionPane.showMessageDialog(  null,
+                    "Look and feel instantiation failure.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (IllegalAccessException e) {
+            JOptionPane.showMessageDialog(  null,
+                    "Look and feel illegal access.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //FIN DEFINITION DU STYLE
+    
     /**
      *
      */
