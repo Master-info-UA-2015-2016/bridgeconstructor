bf4b48d7edacef9dcbea4fabe3fcf5856dda59d2
jfourmond <jerome@fourmond.fr>
2015-11-01 23:05:39 +0100

    Enum Material n'est plus dans Bridge

---
+++ src/bridgeconstructor/Material.java
@@ -0,0 +1,28 @@
+package bridgeconstructor;
+
+public enum Material {
+	/**
+	 *
+	 */
+	Steel,
+
+	/**
+	 *
+	 */
+	Wood,
+
+	/**
+	 *
+	 */
+	Rock,
+
+	/**
+	 *
+	 */
+	Concrete,
+
+	/**
+	 *
+	 */
+	Cord
+}
