2c33462762a79c3b9d7aed4f639e015f6677c3f6
jfourmond <jerome@fourmond.fr>
2015-11-02 12:06:22 +0100

    Ajout Classe Constante

---
+++ src/bridgeconstructor/Constante.java
@@ -0,0 +1,17 @@
+package bridgeconstructor;
+
+public final class Constante {
+	// TODO Constante Acier
+	
+	//	Constante Bois
+	private final float wood_length = 2000;
+	private final float wood_width = 2000;
+	private final float wood_height = 2000;
+	private final float coef_wood = 1;
+	
+	// TODO Constante Béton
+	
+	// TODO Constante Corde
+	
+	// TODO Constante Pierre
+}
