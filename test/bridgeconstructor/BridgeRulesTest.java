2c8fcbda0bc1a8b3c8544ee7df08c00d0b19316d
flodavid <fl.david.53@gmail.com>
2016-01-13 16:46:49 +0100

    ajout de tests

---
+++ test/bridgeconstructor/BridgeRulesTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridgeconstructor;
+
+import expertsystem.RulesBase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Florian
+ */
+public class BridgeRulesTest {
+    
+    public BridgeRulesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of initFromXML method, of class BridgeRules.
+     */
+    @Test
+    public void testInitFromXML() {
+        System.out.println("initFromXML");
+        String filename = "";
+        BridgeRules.initFromXML(filename);
+    }
+
+    /**
+     * Test of initRulesBase method, of class BridgeRules.
+     */
+    @Test
+    public void testInitRulesBase() {
+        System.out.println("initRulesBase");
+        String filename = "";
+        RulesBase result = BridgeRules.initRulesBase(filename);
+        assertNotNull(result);
+    }
+    
+}
